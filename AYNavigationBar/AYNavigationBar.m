3ef9bd7932788be6b03262fca5f0a3fb20ae4b30
高翔 <gao497868860@163.com>
2017-12-01 14:08:37 +0800

    **Commit**

--- AYNavigationBar/AYNavigationBar.m
+++ AYNavigationBar/AYNavigationBar.m
@@ -686,8 +686,6 @@ const CGFloat AYNavigationBarShowLargeTitleViewDuration = 0.5;
 @end
 
 #pragma mark - UIViewController (AYNavigationBar)
-typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewController, BOOL animated);
-
 @implementation UIViewController (AYNavigationBar)
 
 + (void)load
@@ -731,8 +729,6 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
 {
     [self ay__viewWillAppear:animated];
     
-    !self.ay_viewWillAppearBlock ?: self.ay_viewWillAppearBlock(self, animated);
-    
     if (self.navigationController) {
         
         if (!self.navigationController.ay_navigationBarEnabled) {
@@ -771,7 +767,7 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
         self.ay_navigationBar.shadowImage = self.navigationController.ay_barShadowImage;
     }
     self.ay_navigationBar.hidden = self.navigationController.ay_navigationBarHidden;
-    self.ay_prefersNavigationBarHidden = !self.navigationController.ay_navigationBarHidden;
+    self.fd_prefersNavigationBarHidden = !self.navigationController.ay_navigationBarHidden;
     [self.view addSubview:self.ay_navigationBar];
 }
 
@@ -848,26 +844,6 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
     objc_setAssociatedObject(self, @selector(ay_navigationItem), ay_navigationItem, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-- (BOOL)ay_prefersNavigationBarHidden
-{
-    return [objc_getAssociatedObject(self, _cmd) boolValue];
-}
-
-- (void)setAy_prefersNavigationBarHidden:(BOOL)ay_prefersNavigationBarHidden
-{
-    objc_setAssociatedObject(self, @selector(ay_prefersNavigationBarHidden), @(ay_prefersNavigationBarHidden), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (_AYViewControllerWillAppearBlock)ay_viewWillAppearBlock
-{
-    return objc_getAssociatedObject(self, _cmd);
-}
-
-- (void)setAy_viewWillAppearBlock:(_AYViewControllerWillAppearBlock)ay_viewWillAppearBlock
-{
-    objc_setAssociatedObject(self, @selector(ay_viewWillAppearBlock), ay_viewWillAppearBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
-}
-
 @end
 
 #pragma mark - UINavigationController (AYNavigationBar)
@@ -877,7 +853,7 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSArray *sels = @[@"setNavigationBarHidden:animated:", @"pushViewController:animated:"];
+        NSArray *sels = @[@"setNavigationBarHidden:animated:"];
         [sels enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             Method originalMethod = class_getInstanceMethod(self, NSSelectorFromString(obj));
             NSString *swizzledSel = [@"ay__" stringByAppendingString:obj];
@@ -897,35 +873,6 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
     
     if (!hidden) {
         self.topViewController.ay_navigationBar.hidden = YES;
-    }
-}
-
-- (void)ay__pushViewController:(UIViewController *)viewController animated:(BOOL)animated
-{
-    [self ay_setupViewControllerBasedNavigationBarAppearanceIfNeeded:viewController];
-    
-    [self ay__pushViewController:viewController animated:animated];
-}
-
-- (void)ay_setupViewControllerBasedNavigationBarAppearanceIfNeeded:(UIViewController *)appearingViewController
-{
-    if (self.ay_basedNavigationBarAppearanceDisabled) {
-        return;
-    }
-    
-    __weak typeof(self) weakSelf = self;
-    _AYViewControllerWillAppearBlock block = ^(UIViewController *viewController, BOOL animated) {
-        __strong typeof(weakSelf) strongSelf = weakSelf;
-        if (strongSelf) {
-            [strongSelf setNavigationBarHidden:viewController.ay_prefersNavigationBarHidden animated:animated];
-        }
-    };
-    
-    appearingViewController.ay_viewWillAppearBlock = block;
-    
-    UIViewController *disappearingViewController = self.topViewController;
-    if (disappearingViewController && !disappearingViewController.ay_viewWillAppearBlock) {
-        disappearingViewController.ay_viewWillAppearBlock = block;
     }
 }
 
@@ -996,7 +943,7 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
 {
     objc_setAssociatedObject(self, @selector(ay_navigationBarHidden), @(ay_navigationBarHidden), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     [self.topViewController.ay_navigationBar setHidden:ay_navigationBarHidden];
-    self.topViewController.ay_prefersNavigationBarHidden = !ay_navigationBarHidden;
+    self.topViewController.fd_prefersNavigationBarHidden = !ay_navigationBarHidden;
 }
 
 - (NSDictionary<NSAttributedStringKey,id> *)ay_titleTextAttributes
@@ -1010,130 +957,9 @@ typedef void (^_AYViewControllerWillAppearBlock)(UIViewController *viewControlle
     self.topViewController.ay_navigationItem.titleTextAttributes = ay_titleTextAttributes;
 }
 
-- (BOOL)ay_basedNavigationBarAppearanceDisabled
-{
-    return [objc_getAssociatedObject(self, _cmd) boolValue];
-}
-
-- (void)setAy_basedNavigationBarAppearanceDisabled:(BOOL)ay_basedNavigationBarAppearanceDisabled
-{
-    objc_setAssociatedObject(self, @selector(ay_basedNavigationBarAppearanceDisabled), @(ay_basedNavigationBarAppearanceDisabled), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
 - (AYNavigationBar *)ay_navigationBar
 {
     return self.topViewController.ay_navigationBar;
-}
-
-@end
-
-@interface _FDFullscreenPopGestureRecognizerDelegate : NSObject <UIGestureRecognizerDelegate>
-
-@property (nonatomic, weak) UINavigationController *navigationController;
-
-@end
-
-@implementation _FDFullscreenPopGestureRecognizerDelegate
-
-- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer
-{
-    // Ignore when no view controller is pushed into the navigation stack.
-    if (self.navigationController.viewControllers.count <= 1) {
-        return NO;
-    }
-    
-    // Disable when the active view controller doesn't allow interactive pop.
-    UIViewController *topViewController = self.navigationController.viewControllers.lastObject;
-    if (topViewController.fd_interactivePopDisabled) {
-        return NO;
-    }
-    
-    // Ignore pan gesture when the navigation controller is currently in transition.
-    if ([[self.navigationController valueForKey:@"_isTransitioning"] boolValue]) {
-        return NO;
-    }
-    
-    // Prevent calling the handler when the gesture begins in an opposite direction.
-    CGPoint translation = [gestureRecognizer translationInView:gestureRecognizer.view];
-    if (translation.x <= 0) {
-        return NO;
-    }
-    
-    return YES;
-}
-
-@end
-
-@implementation UINavigationController (FDFullscreenPopGesture)
-
-+ (void)load
-{
-    // Inject "-pushViewController:animated:"
-    Method originalMethod = class_getInstanceMethod(self, @selector(pushViewController:animated:));
-    Method swizzledMethod = class_getInstanceMethod(self, @selector(fd_pushViewController:animated:));
-    method_exchangeImplementations(originalMethod, swizzledMethod);
-}
-
-- (void)fd_pushViewController:(UIViewController *)viewController animated:(BOOL)animated
-{
-    if (![self.interactivePopGestureRecognizer.view.gestureRecognizers containsObject:self.fd_fullscreenPopGestureRecognizer]) {
-        
-        // Add our own gesture recognizer to where the onboard screen edge pan gesture recognizer is attached to.
-        [self.interactivePopGestureRecognizer.view addGestureRecognizer:self.fd_fullscreenPopGestureRecognizer];
-        
-        // Forward the gesture events to the private handler of the onboard gesture recognizer.
-        NSArray *internalTargets = [self.interactivePopGestureRecognizer valueForKey:@"targets"];
-        id internalTarget = [internalTargets.firstObject valueForKey:@"target"];
-        SEL internalAction = NSSelectorFromString(@"handleNavigationTransition:");
-        self.fd_fullscreenPopGestureRecognizer.delegate = self.fd_popGestureRecognizerDelegate;
-        [self.fd_fullscreenPopGestureRecognizer addTarget:internalTarget action:internalAction];
-        
-        // Disable the onboard gesture recognizer.
-        self.interactivePopGestureRecognizer.enabled = NO;
-    }
-    
-    // Forward to primary implementation.
-    [self fd_pushViewController:viewController animated:animated];
-}
-
-- (_FDFullscreenPopGestureRecognizerDelegate *)fd_popGestureRecognizerDelegate
-{
-    _FDFullscreenPopGestureRecognizerDelegate *delegate = objc_getAssociatedObject(self, _cmd);
-    
-    if (!delegate) {
-        delegate = [[_FDFullscreenPopGestureRecognizerDelegate alloc] init];
-        delegate.navigationController = self;
-        
-        objc_setAssociatedObject(self, _cmd, delegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    }
-    return delegate;
-}
-
-- (UIPanGestureRecognizer *)fd_fullscreenPopGestureRecognizer
-{
-    UIPanGestureRecognizer *panGestureRecognizer = objc_getAssociatedObject(self, _cmd);
-    
-    if (!panGestureRecognizer) {
-        panGestureRecognizer = [[UIPanGestureRecognizer alloc] init];
-        panGestureRecognizer.maximumNumberOfTouches = 1;
-        
-        objc_setAssociatedObject(self, _cmd, panGestureRecognizer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    }
-    return panGestureRecognizer;
-}
-
-@end
-
-@implementation UIViewController (FDFullscreenPopGesture)
-
-- (BOOL)fd_interactivePopDisabled
-{
-    return [objc_getAssociatedObject(self, _cmd) boolValue];
-}
-
-- (void)setFd_interactivePopDisabled:(BOOL)disabled
-{
-    objc_setAssociatedObject(self, @selector(fd_interactivePopDisabled), @(disabled), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
---
+++ AYNavigationBarDemo/Pods/Target Support Files/Pods-AYNavigationBarDemo/Pods-AYNavigationBarDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AYNavigationBarDemo : NSObject
+@end
+@implementation PodsDummy_Pods_AYNavigationBarDemo
+@end
